4314326b7ede115e8f0535f72a64afeec515109a
kistoo <tomio.nakamura15@gmail.com>
2022-10-06 12:29:15 -0500

    Add game function

--- script.js
+++ script.js
@@ -9,16 +9,33 @@ function getPlayerChoice(){
     //Return that value
 }
 //Create function for the match which asks for 2 choices
-function playRound(playerSelection, computerChoice){
+function playRound(playerSelection, computerSelection){
     //If the choices are the same tell the user it's a draw
     //Evaluate everyelse choices where are not the same and tell the user if won or lost
 }
 //Create function game
 function game(){
+    let wins = 0;
+    let loses = 0;
+    let winner = "";
     //Play 5 rounds of rock paper scissors
     for (let i = 0; i < 5; i++) {
-        getComputerChoice();
-
+       computerSelection = getComputerChoice();
+       playerSelection = getPlayerChoice();
+       winner = playRound(playerSelection, computerSelection);
+       //Count the number of wins and loses
+       if (winner.includes("Win")){
+        wins++;
+       }
+       else{
+        loses++;
+       }
+    }
+    if (wins>loses) {
+        console.log("You won with "+wins+" wins and "+loses+" loses");
+    }
+    else {
+        console.log("You lost with "+wins+" wins and "+loses+" loses");
     }
 }
 //Create variables for the ia and player choices
