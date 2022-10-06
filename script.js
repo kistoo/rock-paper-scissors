0ee0a224d8d27fdc63c5a3488cc12ed8d9e12bbc
kistoo <tomio.nakamura15@gmail.com>
2022-10-06 12:16:43 -0500

    Create functions for the game

---
+++ script.js
@@ -0,0 +1,28 @@
+//Create function for computer choice
+function getComputerChoice(){
+    //Select a random option between Rock, paper, scissors
+    //Return that value
+}
+//Create function for player choice
+function getPlayerChoice(){
+    //Ask the user for a choice between rock paper scissors
+    //Return that value
+}
+//Create function for the match which asks for 2 choices
+function playRound(playerSelection, computerChoice){
+    //If the choices are the same tell the user it's a draw
+    //Evaluate everyelse choices where are not the same and tell the user if won or lost
+}
+//Create function game
+function game(){
+    //Play 5 rounds of rock paper scissors
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice();
+
+    }
+}
+//Create variables for the ia and player choices
+let playerSelection = "";
+let computerSelection = "";
+//Play rock paper scissors
+game();
